c9fefe5a9161f8f41d2d05f26188e4049a891e01
Andrew Marchese <aj.marchese@outlook.com>
2024-07-28 12:57:09 -0700

    matching vercel env variable to database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,12 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Use the DATABASE_URL environment variable
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///backend/can_project.db")
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./can_project.db')
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
