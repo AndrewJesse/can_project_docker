efc5580bbcb6eb6e7afb2d14c7d2b56bf3b07ad9
Andrew Marchese <aj.marchese@outlook.com>
2024-07-28 12:37:54 -0700

    db path handled based on env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./can_project.db"
+# Get the environment variable to check the environment (development/production)
+environment = os.getenv("ENVIRONMENT", "development")
+
+# Define the database URL based on the environment
+if environment == "development":
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./can_project.db"
+else:
+    # Use an absolute path to the database file in production
+    SQLALCHEMY_DATABASE_URL = "sqlite:////app/can_project.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
