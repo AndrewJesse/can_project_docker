43e66bc47572ee0513419de31a9fe9a265258af4
Andrew Marchese <aj.marchese@outlook.com>
2024-07-28 10:19:19 -0700

    trying build script for full stack in vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"], 
+    allow_origins=["https://can-project-docker.vercel.app", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
